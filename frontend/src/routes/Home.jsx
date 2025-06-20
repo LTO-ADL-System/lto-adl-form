e1abe945131140ad9287f070294a7b50c0e97ad6
maiei <146412593+marie-criz@users.noreply.github.com>
2025-06-20 09:30:13 +0800

    style(protected pages): temporary layout for UI testing

---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="px-28 min-h-screen bg-red-500 w-full flex items-center justify-center">
+            <div className="flex flex-col items-start bg-amber-200 max-auto gap-5">
+            <h1 className="text-6xl font-bold text-black text-center">HOME</h1>
+            <p className="text-black">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
